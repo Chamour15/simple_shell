7d9f8bfe8174043569615208b5ffee30a3e2907e
Chamour15 <wafae.chamour@gmail.com>
2023-08-12 16:39:55 -0700

    extra

---
+++ extra.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * non_printed - function for non-printed chars.
+ * @usrinput: input string.
+ *
+ * Return: swapped string
+ */
+char *non_printed(char *usrinput)
+{
+	int i = 0;
+
+	while (usrinput[i])
+	{
+		i++;
+	}
+
+	return (usrinput);
+}
