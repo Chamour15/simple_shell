b2155820686a9b6f500413c10f168d6974611d8c
dakhamohammed <isanagy@live.fr>
2023-08-12 15:32:59 -0700

    Update main.c main.h

--- main.c
+++ main.c
@@ -9,89 +9,81 @@
  */
 int main(int ac, char **av)
 {
-    shell_t dcshell, *sh = &dcshell;
-    char* user_input;
-    int shloop, end_of_file;
-    unsigned int i = 0;
-    UNUSED(ac);
+	shell_t dcshell, *sh = &dcshell;
+	char *user_input;
+	int shloop, end_of_file;
+	unsigned int i = 0;
 
-  init_shell(&dcshell, av);
+	UNUSED(ac);
+	init_shell(&dcshell, av);
 
-    for (shloop = 1; shloop == 1;)
-    {
-        write(STDIN_FILENO, "~> ", 4);
-        user_input = line_reader(&end_of_file);
-
-        if (end_of_file != EOF)
-        {
-            if (user_input == NULL)
-            {
-                continue;
-            }
-            shloop = parse_cmd(sh, user_input);
-            sh->line_counter += 1;
-            free(user_input);
-        }
-        else
-        {
-            shloop = 0;
-            free(user_input);
-        }
-    }
-
-    while (sh->_environ[i])
-    {
-        free(sh->_environ[i]);
-        i++;
-    }
-
-    free(sh->pid);
-    free(sh->_environ);
-
-    switch (dcshell.shell_status)
-    {
-    case -1:
-        return (255);
-        break;
-    default:
-        return (dcshell.shell_status);
-        break;
-    }
+	for (shloop = 1; shloop == 1;)
+	{
+		write(STDIN_FILENO, "~> ", 4);
+		user_input = line_reader(&end_of_file);
+		if (end_of_file != EOF)
+		{
+			if (user_input == NULL)
+				continue;
+			shloop = parse_cmd(sh, user_input);
+			sh->line_counter += 1;
+			free(user_input);
+		}
+		else
+		{
+			shloop = 0;
+			free(user_input);
+		}
+	}
+	while (sh->_environ[i])
+	{
+		free(sh->_environ[i]);
+		i++;
+	}
+	free(sh->pid);
+	free(sh->_environ);
+	switch (dcshell.shell_status)
+	{
+		case -1:
+			return (255);
+		default:
+			return (dcshell.shell_status);
+	}
 }
 
 /**
  * init_shell - Initialize shell structure.
  * @dcshell: shell structure.
  * @av: command line arguments.
- * 
- * Return: void, no return
+ *
+ * Return: void, no return.
  */
 void init_shell(shell_t *dcshell, char **av)
 {
-    unsigned int i;
+	unsigned int i;
 
-    dcshell->av = av;
-    dcshell->shell_status = 0;
-    dcshell->user_input = NULL;
-    dcshell->line_counter = 1;
-    dcshell->args = NULL;
+	dcshell->av = av;
+	dcshell->shell_status = 0;
+	dcshell->user_input = NULL;
+	dcshell->line_counter = 1;
+	dcshell->args = NULL;
 
-    i = 0;
-    while (environ[i])
-    {
-        i++;
-    }
+	i = 0;
+	while (environ[i])
+	{
+		i++;
+	}
 
-    dcshell->_environ = malloc((i + 1) * sizeof(char*));
+	dcshell->_environ = malloc((i + 1) * sizeof(char *));
 
-    i = 0;
-    while (environ[i])
-    {
-        dcshell->_environ[i] = _strdup(environ[i]);
-        i++;
-    }
+	i = 0;
+	while (environ[i])
+	{
+		dcshell->_environ[i] = _strdup(environ[i]);
+		i++;
+	}
 
-    dcshell->_environ[i] = NULL;
-    dcshell->pid = _itoa(getpid());
+	dcshell->_environ[i] = NULL;
+	dcshell->pid = _itoa(getpid());
 }
 
--- main.h
+++ main.h
@@ -1,4 +1,100 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <limits.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <string.h>
+
+#define UNUSED(x) (void)(x)
+
+extern char **environ;
+
+typedef struct shell_s shell_t;
+typedef struct builtins_s builtins_t;
+
+/**
+ * struct shell_s - shell_s struct.
+ * @av: commad line arguments.
+ * @user_input: command line of the user.
+ * @args: command line arguments.
+ * @shell_status: shell status.
+ * @line_counter: lines counter.
+ * @_environ: environment variables.
+ * @pid: process id of the shell.
+ */
+struct shell_s
+{
+	char **av;
+	char *user_input;
+	char **args;
+	char *pid;
+	char **_environ;
+	int shell_status;
+	int line_counter;
+};
+
+/**
+ * struct builtins_s - Builtin struct for command args.
+ * @command_name: The name of the command builtin.
+ * @function: data type pointer function.
+ */
+struct builtins_s
+{
+	char *command_name;
+	int (*function)(shell_t *dcshell);
+};
+
+/* scanner.c */
+char *line_reader(int *end_of_file);
+
+/* string_functions.c*/
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, const char *src);
+char *_strdup(const char *str);
+int _strcmp(char *s1, char *s2);
+
+/* string_functions2.c*/
+int number_length(int num);
+int _strlen(const char *str);
+char *_itoa(int num);
+int _atoi(char *s);
+int _isdigit(const char *c);
+
+/* memory_functions.c */
+void _memcpy(void *destptr, const void *srcptr, unsigned int sizep);
+void *_realloc(void *oldptr, unsigned int old_msize, unsigned int new_msize);
+char **_reallocmdp(char **oldmp, unsigned int old_size, unsigned int new_size);
+
+/* command_executer.c*/
+int find_command(shell_t *dcshell);
+int execute_cmd(shell_t *dcshell);
+int isexe(shell_t *dcshell);
+char *_cmd_location(char *cmd, char **_environ);
+int isdir(char *path, int *i);
+
+/* environment.c */
+char *_envir(const char *envname, char **_environ);
+
+/* command_parser.c */
+int parse_cmd(shell_t *dcshell, char *usrinput);
+char **parse_cmdline(char *usrinput);
+
+/* command_parser2.c*/
+char *non_printed(char *usrinput);
+
+/* errors_handler.c */
+int chk_cmd_err(char *directory, shell_t *dcshell);
+int errors(shell_t *dcshell, int error);
+char *not_found404(shell_t *dcshell);
+
+/* builtins_functions.c*/
+int (*builtins(char *command)) (shell_t *dcshell);
+int shell_exit(shell_t *dcshel);
+
 #endif
+
